70790f81092402d13f1853d5629bb645adf8c1e7
Osariusz <osariusz@interia.pl>
2023-07-22 16:35:58 +0200

    Topic display prototype

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import './App.css';
 import { DataGrid } from '@mui/x-data-grid';
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+import { Container } from "@mui/material";
+
+const Wrapper = styled(Box)({
+  display: "grid",
+  gridTemplateColumns: "repeat(1, 1fr)",
+  gridGap: 80,
+  marginTop: 30
+});
 
 function getSelectionWidth(){
   return 3;
@@ -15,6 +25,10 @@ function getSelectionSymbols(){
   [{value: "one", path: "one.png"}, {value: "two", path: "two.png"}, {value: "three", path: "three.png"}],
   [{value: "circle", path: "circle.png"}, {value: "square", path: "square.png"}, {value: "triangle", path: "triangle.png"}]
   ];
+}
+
+function getTopics(){
+  return [{value: "elephant", path: "elephant.png"},{value: "kettle", path: "kettle.png"},{value: "grass", path: "grass.png"},{value: "knife", path: "knife.png"}]
 }
 
 function generateColumns(width){
@@ -37,14 +51,34 @@ function generateRows(height, columns, selectionSymbols){
   return rows;
 }
 
-function siteContent(){
+function symbolSelectionComponent(){
   let selectionSymbols = getSelectionSymbols();
   let columns = generateColumns(getSelectionWidth());
   let rows = generateRows(getSelectionHeight(),columns, selectionSymbols);
   return (
-    <div>
       <DataGrid slots={{columnHeaders: () => null}} hideFooter rows={rows} columns={columns}></DataGrid>
-    </div>
+  )
+}
+
+function topicComponent(){
+  let rows = []
+  let topics = getTopics()
+  for(let i = 0;i<topics.length;++i){
+    rows.push({id: i, 'col1': topics[i].path})
+  }
+  return (
+    <DataGrid slots={{columnHeaders: () => null}} hideFooter rows={rows} columns={[{field: 'col1', width: 150}]}></DataGrid>
+  )
+}
+
+function siteContent(){
+  return (
+    <Container>
+      <Wrapper>
+        <Box>{topicComponent()}</Box>
+        <Box>{symbolSelectionComponent()}</Box>
+      </Wrapper>
+    </Container>
   )
 }
 

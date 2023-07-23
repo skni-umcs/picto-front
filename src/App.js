72c5ccc8dd0418eabe366abbfb8d12732cbf0f6b
Osariusz <osariusz@interia.pl>
2023-07-23 17:23:40 +0200

    Submit and symbol buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
-import { Container } from "@mui/material";
+import { Container, ToggleButton } from "@mui/material";
+import Button from "@mui/material/Button"
+import { ToggleButtons } from './ToggleButtons.js'
 
 const Wrapper = styled(Box)({
   display: "grid",
   gridTemplateColumns: "repeat(1, 1fr)",
-  gridGap: 80,
+  gridGap: '75%',
   marginTop: 30
 });
 
@@ -18,8 +20,8 @@ function getSelectionHeight(){
   return 3;
 }
 
-function getSelectionSymbols(){
-  return [
+function getSelectionSymbols(){ 
+  return [ //throws an error but these are only placeholder values
   [{value: "black", path: "black.png"}, {value: "white", path: "white.png"}, {value: "gray", path: "gray.png"}],
   [{value: "one", path: "one.png"}, {value: "two", path: "two.png"}, {value: "three", path: "three.png"}],
   [{value: "circle", path: "circle.png"}, {value: "square", path: "square.png"}, {value: "triangle", path: "triangle.png"}]
@@ -27,18 +29,29 @@ function getSelectionSymbols(){
 }
 
 function getTopics(){
-  return [{value: "elephant", path: "elephant.png", chosen: false},{value: "kettle", path: "kettle.png", chosen: true},{value: "grass", path: "grass.png", chosen: false},{value: "knife", path: "knife.png", chosen: false}]
+  return [ //throws an error but these are only placeholder values
+    {value: "elephant", path: "elephant.png", chosen: false},
+    {value: "kettle", path: "kettle.png", chosen: true},
+    {value: "grass", path: "grass.png", chosen: false},
+    {value: "knife", path: "knife.png", chosen: false}
+  ]
 }
+
+function submit(){
+  console.log("backend where")
+}
+
+
+
 
 function generateRows(height, width, selectionSymbols){
   let rows = [];
   for(let j = 0;j<width;++j){
     let columnContent = []
     for(let i = 0;i<height;++i){
-      let toAdd = <Box>{selectionSymbols[j][i].path}</Box>;
-      columnContent.push(toAdd);
+      columnContent.push(selectionSymbols[j][i]);
     }
-    rows.push(<Box>{columnContent}</Box>)
+    rows.push(ToggleButtons(columnContent))
   }
   return rows;
 }
@@ -47,15 +60,19 @@ function symbolSelectionComponent(){
   let selectionSymbols = getSelectionSymbols();
   let rows = generateRows(getSelectionHeight(),getSelectionWidth(), selectionSymbols);
   return (
-      <Box sx={{
-        display: 'flex',
-        flexWrap: 'nowrap',
-        p: 1,
-        m: 1,
-        bgcolor: 'background.paper',
-        borderRadius: 1,
-        justifyContent : 'space-between'
-      }}>{rows}</Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'nowrap',
+          p: 1,
+          m: 1,
+          bgcolor: 'background.paper',
+          borderRadius: 1,
+          justifyContent : 'space-between'
+        }}
+      >
+        {rows}
+      </Box>
   )
 }
 
@@ -71,24 +88,33 @@ function topicComponent(){
     }
   }
   return (
-    <Box sx={{
-      display: 'flex',
-      flexWrap: 'nowrap',
-      p: 1,
-      m: 1,
-      bgcolor: 'background.paper',
-      borderRadius: 1,
-      justifyContent : 'space-between'
-    }}>{rows}</Box>
+    <Box 
+      sx={{
+        display: 'flex',
+        flexWrap: 'nowrap',
+        p: 1,
+        m: 1,
+        bgcolor: 'background.paper',
+        borderRadius: 1,
+        justifyContent : 'space-between'
+      }}
+    >
+      {rows}
+    </Box>
   )
+}
+
+function submitComponent(){
+  return <Button onClick={submit}>submit</Button>;
 }
 
 function siteContent(){
   return (
-    <Container>
+    <Container maxWidth={false}>
       <Wrapper>
         <Box>{topicComponent()}</Box>
         <Box>{symbolSelectionComponent()}</Box>
+        <Box sx={{display:'flex', justifyContent: 'center'}}>{submitComponent()}</Box>
       </Wrapper>
     </Container>
   )

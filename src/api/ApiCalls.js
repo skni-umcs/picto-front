e51bc84c83a3ccb6574a4e4f97b42c3ac81ffbdf
Osariusz <osariusz@interia.pl>
2023-09-12 23:06:30 +0200

    Backend ip

--- src/api/ApiCalls.js
+++ src/api/ApiCalls.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BACKEND_IP = 'http://localhost:8080';
+export const BACKEND_IP = window.location.protocol+window.location.hostname+"/api";
 
 export const backend = axios.create({
   baseURL: BACKEND_IP,

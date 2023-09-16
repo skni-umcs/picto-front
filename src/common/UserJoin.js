83d7a3c1e066441444ff33d36d4b1dce42c5b0db
Osariusz <osariusz@interia.pl>
2023-09-16 11:42:00 +0200

    Agree panel

--- src/common/UserJoin.js
+++ src/common/UserJoin.js
@@ -1,13 +1,29 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import {useRef, useState} from 'react';
+import React, {useRef, useState} from 'react';
+import {Checkbox} from '@mui/material';
 
 function UserJoin({joinUser}) {
   const [gameId, setGameId] = useState(0);
   const buttonRef = useRef(null);
+  const [agreePanel, setAgreePanel] = useState(false);
 
   function onJoin() {
+    setAgreePanel(false);
     joinUser(gameId);
+  }
+
+  function setAgreePanelClick() {
+    setAgreePanel(true);
+  }
+
+  function AgreeComponent() {
+    return (
+        <Box className="agreeComponent">
+          <Checkbox onClick={onJoin}></Checkbox>
+          Wyrażam zgodę na udział w badaniu
+        </Box>
+    );
   }
 
   const handleKeyPress = e => {
@@ -16,15 +32,17 @@ function UserJoin({joinUser}) {
     }
   }
 
-  return (
-      <Box sx={{flexDirection: 'column', display: 'flex'}}
-           className={'joinComponent'}>
-        <Box className="joinComponentsContainer">
-          <input onKeyDown={handleKeyPress} className="joinInput" value={gameId}
-                 onChange={e => setGameId(e.target.value)}></input>
-          <Button ref={buttonRef} className="joinButton" onClick={onJoin} autofocus>DOŁĄCZ</Button>
-        </Box>
+  return (<>
+      {agreePanel && <AgreeComponent/>}
+      {!agreePanel && <Box sx={{flexDirection: 'column', display: 'flex'}}
+                               className={'joinComponent'}>
+      <Box className="joinComponentsContainer">
+        <input onKeyDown={handleKeyPress} className="joinInput" value={gameId}
+               onChange={e => setGameId(e.target.value)}></input>
+        <Button ref={buttonRef} className="joinButton" onClick={setAgreePanelClick} autofocus>DOŁĄCZ</Button>
       </Box>
+    </Box>}
+  </>
   );
 }
 
